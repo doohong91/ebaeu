d676715b87788ed40d1a2e6702a555f889185075
JongMin (5285) <jongm711@gmail.com>
2019-05-15 09:54:08 +0900

    영화 봤어요/안봤어요 기능 버튼 구현

--- actors/views.py
+++ actors/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Actor, Movie, Genre, Rating
 from .serializers import MovieSerializer, ActorSerializer, GenreSerializer, RatingSerializer
+from django.contrib.auth import get_user_model
 
 def index(request):
   actors = Actor.objects.filter(id__lt=10)
@@ -15,6 +16,7 @@ def detail(request, actor_id):
   actor = get_object_or_404(Actor,pk=actor_id)
   return render(request,'actors/detail.html',{'actor':actor})
 
+@login_required  
 def like_actor(request, actor_id):
   actor = get_object_or_404(Actor,pk=actor_id)
   if request.user in actor.like_users.all():
@@ -22,3 +24,18 @@ def like_actor(request, actor_id):
   else:
     actor.like_users.add(request.user)
   return redirect('actors:detail',actor_id)
+
+@login_required  
+def viewed_movie(request, actor_id, movie_id):
+  movie = get_object_or_404(Movie,pk=movie_id)
+  if request.user in movie.viewed_users.all():
+    movie.viewed_users.remove(request.user)
+  else:
+    movie.viewed_users.add(request.user)
+  return redirect('actors:detail',actor_id)
+
+@login_required  
+def rcmd_movie(request):
+  user=request.user
+  like_actors=user.like_actors.all()
+  return render(request,'actors/recommand.html',{'like_actors':like_actors})
