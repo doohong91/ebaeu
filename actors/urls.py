9a1ef2f96c3b59c5b0cd6f47c2bc61f77ec5cd6d
JongMin (Mul) <jongm711@gmail.com>
2019-05-15 16:40:19 +0900

    봤어요 수정

--- actors/urls.py
+++ actors/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('',views.index, name="main"),
     path('actors/<int:actor_id>',views.detail, name="detail"),
     path('actors/<int:actor_id>/like',views.like_actor, name="like"),
-    path('actors/<int:movie_id>',views.viewed_movie, name="watched"),
-    path('recommand',views.rcmd_movie,name="recommand")
+    path('actors/<int:actor_id>/<int:movie_id>/',views.viewed_in_detail, name="viewed_in_detail"),
+    path('recommand',views.rcmd_movie,name="recommand"),
+    path('recommand/<int:movie_id>',views.viewed_in_recommand, name="viewed_in_recommand"),
 ]
